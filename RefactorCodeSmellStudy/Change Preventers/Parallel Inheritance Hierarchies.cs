20d010a46c9f67a7688cda0a4749a8ecd34a8af8
shift <qjfrntop12@naver.com>
2019-08-06 14:43:36 +0900

    Add Change Preventers

---
+++ RefactorCodeSmellStudy/Change Preventers/Parallel Inheritance Hierarchies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorCodeSmellStudy.Change_Preventers
+{
+    class Bad
+    {
+        public interface Engineer
+        {
+            MileStone MileStone
+            {
+                get;
+                set;
+            }
+        }
+        public interface MileStone { }
+
+        public class ComputerEngineer : Engineer { }
+        public class ComputerMileStone : MileStone { }
+    }
+}
--- RefactorCodeSmellStudy/Change Preventers/Shotgun Surgery.cs
+++ RefactorCodeSmellStudy/Change Preventers/Shotgun Surgery.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace RefactorCodeSmellStudy.Change_Preventers
 {
-    class Shotgun_Surgery
+    class Bad
     {
+        // These three class doing common behaiver "login management" It need to be
+        class SessionManager
+        {
+            public List<string> Sessions;
+            public void ExtendSession(int time) { }
+        }
+
+        class LoginManager
+        {
+            public void Login(string id, string password) { }
+        }
+        
+        class LogoutManager
+        {
+            public void Logout(string id, string password) { }
+        }
+    }
+
+    class Good
+    {
+        class LoginManager
+        {
+            public List<string> Sessions;
+
+            public void Login(string id, string password) { }
+            public void Logout(string id, string password) { }
+            public void ExtendSession(int time) { }
+        }
     }
 }
